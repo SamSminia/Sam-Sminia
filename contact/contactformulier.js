a18279f5896d70808447b945092bffef96888bff
Sam Sminia <124161381+SamSminia@users.noreply.github.com>
2023-07-31 16:07:52 +0200

    Delete contact directory

--- contact/contactformulier.js
+++
@@ -1,9 +0,0 @@
-const contactForm = document.getElementById('contactForm');
-
-contactForm.addEventListener('submit', function (event) {
-  if (!contactForm.checkValidity()) {
-    // Voorkom dat het formulier wordt verzonden als niet alle velden zijn ingevuld
-    event.preventDefault();
-    alert('Vul alle verplichte velden in.');
-  }
-})            
